851de9371c74646789980b9c72add8dd24dd5779
mennatallah nasr <91540503+Mennah1@users.noreply.github.com>
2025-11-20 11:37:33 +0200

    Update database.js

--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -4,9 +4,9 @@ const config = require('../config/env');
 const connectDB = async () => {
     try {
         await mongoose.connect(config.MONGO_URI);
-        console.log('✅ MongoDB Connected Successfully');
+        console.log(' MongoDB Connected Successfully');
     } catch (error) {
-        console.error('❌ MongoDB Connection Error:', error);
+        console.error('MongoDB Connection Error:', error);
         process.exit(1);
     }
 };
